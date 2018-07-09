69ddb74f9261655641134ab9e2ebe2863918820f
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-07-09 15:14:37 -0400

    hello

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Hello from './Hello'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: 'Malik'
+    }
+  }
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className='App'>
+        <p>
+          <Hello name={this.state.username} />
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
